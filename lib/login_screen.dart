baa29cbca2ed04951e3813a8f99caa32873dd713
devanandp <deva051195@gmail.com>
2019-11-21 18:40:25 +0530

    Added First Assignment

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,49 +1,47 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+
 class LoginScreen extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: SafeArea(
-          child: Scaffold(
-            appBar: AppBar(
-              title: Center(
-                  child: Text("Healthy India")),
-                  backgroundColor: Colors.redAccent,
-            ),
-                  body: Column(
-                    children: <Widget>[
-                      Container(
-                        color: Colors.orange,
-                        child: FlutterLogo(
-                          size: 60.0,
-                        ),
-                      ),
-                      Container(
-                        color: Colors.blue,
-                        child: FlutterLogo(
-                          size: 60.0,
-                        ),
-                      ),
-                      Container(
-                        color: Colors.purple,
-                        child: FlutterLogo(
-                          size: 60.0,
-                        ),
-                      ),
+        home: Container(
+      child: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Center(child: Text("Healthy India")),
+            backgroundColor: Colors.redAccent,
+          ),
+          body: Column(
+            children: <Widget>[
               Center(
-          child: CircleAvatar(
-          radius: 100,
-          backgroundImage: AssetImage('assets/images/wp2432582.jpg'),
+                child: CircleAvatar(
+                  radius: 60,
+                  backgroundImage: AssetImage('assets/images/wp2432582.jpg'),
+                ),
+              ),
+              Center(
+                child: Container(
+                    height: 200, child: Text("Welcome to Health India App")),
+              ),
+              Center(
+                child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter the Username')),
+              ),
+              Center(
+                child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter the Password')),
+              ),
+            ],
           ),
-          ),
-
-                    ],
-                  ),
-          ),
-        )
-    );
+        ),
+      ),
+    ));
   }
 }
