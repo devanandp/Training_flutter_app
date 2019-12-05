94ac3d86fbe79cba377353c36c9d9f7f7c0bf132
devanandp <deva051195@gmail.com>
2019-12-05 15:14:02 +0530

    Assignment 5 Code added

---
+++ lib/bmi_calculator.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+
+class Bmi_Calculator extends StatefulWidget {
+  @override
+  _Bmi_CalculatorState createState() {
+    return _Bmi_CalculatorState();
+  }
+}
+
+class _Bmi_CalculatorState extends State<Bmi_Calculator> {
+  var bmi = 0.0;
+  final textcontroller1 = TextEditingController();
+  final textcontroller2 = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: SafeArea(
+      child: Scaffold(
+          body: Column(children: <Widget>[
+        Container(
+          child: Image.asset(
+            'assets/images/bmi_about.png',
+            fit: BoxFit.contain,
+          ),
+        ),
+        ListTile(
+          title: Text(
+            'Formula for calculating BMI: Weight/Height*Height',
+            style: TextStyle(color: Colors.deepPurple, fontSize: 15.0),
+          ),
+        ),
+        new Divider(
+          color: Colors.deepPurple,
+          thickness: 5.0,
+        ),
+        Container(
+            child: Row(
+          children: <Widget>[
+            Container(
+              child: Text(
+                'Height in meters',
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+            ),
+            Container(
+              width: 270,
+              padding: EdgeInsets.all(4.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: 'Enter your height', border: InputBorder.none),
+                    controller: textcontroller1,
+              ),
+            )
+          ],
+        )),
+        new Divider(
+          color: Colors.deepPurple,
+          thickness: 5.0,
+        ),
+        Container(
+            child: Row(
+          children: <Widget>[
+            Container(
+              child: Text(
+                'Weight in kg',
+                style: TextStyle(
+                  fontSize: 21,
+                ),
+              ),
+            ),
+            Container(
+              width: 270,
+              padding: EdgeInsets.all(4.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: 'Enter your height', border: InputBorder.none),
+                controller: textcontroller2,
+              ),
+            )
+          ],
+        )),
+        new Divider(
+          color: Colors.deepPurple,
+          thickness: 5.0,
+        ),
+        Container(
+            height: 50,
+            child: Row(
+              children: <Widget>[
+                ButtonTheme(
+                  height: 60,
+                  child: RaisedButton(
+                    child: Text("Calculate BMI"),
+                    color: Colors.deepPurple,
+                    onPressed: () {
+                      setState(() {
+                        bmi = double.parse(textcontroller1.text) / double.parse(textcontroller2.text) ;
+                      });
+                    },
+                  ),
+                ),
+                Container(
+                  child: Text(
+                    "Your BMI is $bmi",
+                    style: TextStyle(
+                      fontSize: 21,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                )
+              ],
+            )),
+        new Divider(
+          color: Colors.deepPurple,
+          thickness: 5.0,
+        ),
+        Container(
+          height: 30,
+          child: Row(),
+        ),
+        new Divider(
+          color: Colors.deepPurple,
+          thickness: 5.0,
+        ),
+      ])),
+    ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:training_app/user_defined.dart';
 import 'call_student.dart';
 import 'home_screen.dart';
 import 'login_screen.dart';
+import 'bmi_calculator.dart';
 void main() =>
-    runApp(HomeScreen());
+    runApp(Bmi_Calculator());
 
 
