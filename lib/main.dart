b7631c3c8d61359ac54732726ee8ec7f9306acfa
devanandp <deva051195@gmail.com>
2019-12-17 13:19:33 +0530

    Colour App added

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:training_app/user_defined.dart';
+import 'package:training_app/ColorApp.dart';
 import 'call_student.dart';
 import 'home_screen.dart';
 import 'login_screen.dart';
 import 'bmi_calculator.dart';
 void main() {
   runApp(MaterialApp(
-    home: LoginScreen()
+    home: ColorApp()
   ));
 }
 
