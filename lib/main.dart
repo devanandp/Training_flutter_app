44acce9028f11b8a90478607a3db08bfcf8303fe
devanandp <deva051195@gmail.com>
2019-12-18 18:47:20 +0530

    Updated with Assignment 1 of Week 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:training_app/color_app.dart';
-import 'package:training_app/rainbow.dart';
+//import 'package:training_app/color_app.dart';
+//import 'package:training_app/rainbow.dart';
+
+import 'form_creation.dart';
 
 void main() {
   runApp(
       MaterialApp(
-    home: Rainbow()
+    home: Formcreator()
   ));
 }
 
